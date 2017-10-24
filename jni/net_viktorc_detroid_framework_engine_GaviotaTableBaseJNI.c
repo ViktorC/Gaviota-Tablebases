26c170276ff982960d27342a436a99c9523ee965
ViktorC <viktor.csomor@gmail.com>
2017-10-25 00:58:22 +0200

    Continued with the JNI header implementation.

--- jni/net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI.c
+++ jni/net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI.c
@@ -11,68 +11,103 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * A struct for holding the board information required for probing.
+ */
+struct BOARD_POS {
+	// The squares occupied by the white and black pieces respectively.
+	unsigned* cWSqrs;
+	unsigned* cBSqrs;
+	// The pieces occupying those squares.
+	unsigned char* cWPcs;
+	unsigned char* cBPcs;
+};
+
+/**
+ * Copies the contents of the Java integer and character arrays into the fields of a BOARD_POS
+ * struct, releases the Java arrays, and returns the struct.
+ */
+struct BOARD_POS getBoardPosition(JNIEnv * env, jintArray wSqrs, jintArray bSqrs, jcharArray wPcs,
+		jcharArray bPcs);
+
+/**
+ * Creates and returns a Java integer array containing the probe results.
+ */
+jintArray getProbeResult(JNIEnv * env, unsigned info, unsigned dtm);
+
 JNIEXPORT jstring JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_init
 		(JNIEnv * env, jobject obj, jboolean verbose, jint compScheme, jstring paths) {
-	const char *cPaths = (*env)->GetStringUTFChars(env, paths, false);
-	const char ** initPaths = tbpaths_init();
+	const char* cPaths = (*env)->GetStringUTFChars(env, paths, false);
+	const char** initPaths = tbpaths_init();
 	initPaths = tbpaths_add(initPaths, cPaths);
-	const char *cInitInfo = tb_init(verbose, compScheme, initPaths);
+	const char* cInitInfo = tb_init(verbose, compScheme, initPaths);
 	jstring initInfo = (*env)->NewStringUTF(env, cInitInfo);
 	(*env)->ReleaseStringUTFChars(env, paths, cPaths);
 	return initInfo;
 }
+
 JNIEXPORT jstring JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_restart
 		(JNIEnv * env, jobject obj, jboolean verbose, jint compScheme, jstring paths) {
-	const char ** initPaths = tbpaths_init();
+	const char** initPaths = tbpaths_init();
 	initPaths = tbpaths_done(initPaths);
-	const char *cPaths = (*env)->GetStringUTFChars(env, paths, false);
+	const char* cPaths = (*env)->GetStringUTFChars(env, paths, false);
 	initPaths = tbpaths_init();
 	initPaths = tbpaths_add(initPaths, cPaths);
-	const char *cInitInfo = tb_restart (verbose, compScheme, initPaths);
+	const char* cInitInfo = tb_restart (verbose, compScheme, initPaths);
 	jstring initInfo = (*env)->NewStringUTF(env, cInitInfo);
 	(*env)->ReleaseStringUTFChars(env, paths, cPaths);
 	return initInfo;
 }
+
 JNIEXPORT jboolean JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_isInit
 		(JNIEnv * env, jobject obj) {
 	return tb_is_initialized();
 }
+
 JNIEXPORT jint JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_availability
 		(JNIEnv * env, jobject obj) {
 	return tb_availability();
 }
+
 JNIEXPORT jlong JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_indexMemory
 		(JNIEnv * env, jobject obj) {
 	return tb_indexmemory();
 }
+
 JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_close
 		(JNIEnv * env, jobject obj) {
 	tb_done();
 	tbpaths_done(tbpaths_init());
 	return;
 }
+
 JNIEXPORT jboolean JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_initCache
 		(JNIEnv * env, jobject obj, jlong size, jint wdlFraction) {
 	return tbcache_init(size, wdlFraction);
 }
+
 JNIEXPORT jboolean JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_restartCache
 		(JNIEnv * env, jobject obj, jlong size, jint wdlFraction) {
 	return tbcache_restart(size, wdlFraction);
 }
+
 JNIEXPORT jboolean JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_isCacheOn
 		(JNIEnv * env, jobject obj) {
 	return tbcache_is_on();
 }
+
 JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_clearCache
 		(JNIEnv * env, jobject obj) {
 	tbcache_flush();
 	return;
 }
+
 JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_closeCache
 		(JNIEnv * env, jobject obj) {
 	tbcache_done();
 	return;
 }
+
 JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_getStats
 		(JNIEnv * env, jobject obj, jlongArray intStats, jdoubleArray fpStats) {
 	unsigned long int wdlEasyHits, wdlHardProb, wdlSoftProb, wdlCacheSize;
@@ -106,74 +141,98 @@ JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBas
 	(*env)->SetDoubleArrayRegion(env, fpStats, 0 , 3, cFpArray);
 	return;
 }
+
 JNIEXPORT void JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_resetStats
 		(JNIEnv * env, jobject obj) {
 	tbstats_reset();
 	return;
 }
+
 JNIEXPORT jintArray JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_probe
 		(JNIEnv * env, jobject obj, jint sideToMove, jint enPassantSqr, jint castlingRights,
 		jintArray wSqrs, jintArray bSqrs, jcharArray wPcs, jcharArray bPcs) {
 	unsigned int info, dtm;
-	jintArray out;
-	unsigned** cWSqrs, cBSqrs;
-	unsigned char** cWPcs, cBPcs;
-	convertJavaArrayArguments(env, wSqrs, bSqrs, wPcs, bPcs, &cWSqrs, &cBSqrs, &cWPcs, &cBPcs);
-	if (tb_probe_hard(sideToMove, enPassantSqr, castlingRights, cWSqrs, cBSqrs, cWPcs, cBPcs, &info, &dtm)) {
-		out =  (*env)->NewIntArray(env, 2);
-		jint res[] = { info, dtm };
-		(*env)->SetIntArrayRegion(env, out, 0 , 2, res);
-	} else {
-		out = NULL;
+	jintArray out = NULL;
+	struct BOARD_POS pos = getBoardPosition(env, wSqrs, bSqrs, wPcs, bPcs);
+	if (tb_probe_hard(sideToMove, enPassantSqr, castlingRights, pos.cWSqrs, pos.cBSqrs,
+			pos.cWPcs, pos.cBPcs, &info, &dtm)) {
+		out = getProbeResult(env, info, dtm);
 	}
 	return out;
 }
+
 JNIEXPORT jintArray JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_probeSoft
 		(JNIEnv * env, jobject obj, jint sideToMove, jint enPassantSqr, jint castlingRights,
 		jintArray wSqrs, jintArray bSqrs, jcharArray wPcs, jcharArray bPcs) {
-
-	return NULL;
+	unsigned int info, dtm;
+	jintArray out = NULL;
+	struct BOARD_POS pos = getBoardPosition(env, wSqrs, bSqrs, wPcs, bPcs);
+	if (tb_probe_soft(sideToMove, enPassantSqr, castlingRights, pos.cWSqrs, pos.cBSqrs,
+			pos.cWPcs, pos.cBPcs, &info, &dtm)) {
+		out = getProbeResult(env, info, dtm);
+	}
+	return out;
 }
+
 JNIEXPORT jint JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_probeWDL
 		(JNIEnv * env, jobject obj, jint sideToMove, jint enPassantSqr, jint castlingRights,
 		jintArray wSqrs, jintArray bSqrs, jcharArray wPcs, jcharArray bPcs) {
-
-	return NULL;
+	unsigned int info;
+	struct BOARD_POS pos = getBoardPosition(env, wSqrs, bSqrs, wPcs, bPcs);
+	if (!tb_probe_WDL_hard(sideToMove, enPassantSqr, castlingRights, pos.cWSqrs, pos.cBSqrs,
+			pos.cWPcs, pos.cBPcs, &info)) {
+		info = tb_UNKNOWN;
+	}
+	return info;
 }
+
 JNIEXPORT jint JNICALL Java_net_viktorc_detroid_framework_engine_GaviotaTableBaseJNI_probeSoftWDL
 		(JNIEnv * env, jobject obj, jint sideToMove, jint enPassantSqr, jint castlingRights,
 		jintArray wSqrs, jintArray bSqrs, jcharArray wPcs, jcharArray bPcs) {
-
-	return NULL;
+	unsigned int info;
+	struct BOARD_POS pos = getBoardPosition(env, wSqrs, bSqrs, wPcs, bPcs);
+	if (!tb_probe_WDL_soft(sideToMove, enPassantSqr, castlingRights, pos.cWSqrs, pos.cBSqrs,
+			pos.cWPcs, pos.cBPcs, &info)) {
+		info = tb_UNKNOWN;
+	}
+	return info;
 }
-/**
- * It copies the contents of the Java integer and char arrays into native C integer and char arrays
- * and releases the Java arrays.
- */
-void convertJavaArrayArguments(JNIEnv * env, jintArray wSqrs, jintArray bSqrs, jcharArray wPcs,
-		jcharArray bPcs, /*@out@*/ unsigned** cWSqrs, /*@out@*/ unsigned** cBSqrs,
-		/*@out@*/ unsigned char** cWPcs, /*@out@*/ unsigned char** cBPcs) {
+
+struct BOARD_POS getBoardPosition(JNIEnv * env, jintArray wSqrs, jintArray bSqrs, jcharArray wPcs,
+		jcharArray bPcs) {
 	int wLength = (*env)->GetArrayLength(env, wSqrs);
 	int bLength = (*env)->GetArrayLength(env, bSqrs);
-	jint *jWSqrs = (*env)->GetIntArrayElements(env, wSqrs, NULL);
-	jint *jBSqrs = (*env)->GetIntArrayElements(env, bSqrs, NULL);
-	jchar *jWPcs = (*env)->GetCharArrayElements(env, wPcs, NULL);
-	jchar *jBPcs = (*env)->GetCharArrayElements(env, bPcs, NULL);
-	cWSqrs[wLength];
-	cBSqrs[bLength];
-	cWPcs[wLength];
-	cBPcs[bLength];
+	jint *jWSqrs = (*env)->GetIntArrayElements(env, wSqrs, false);
+	jint *jBSqrs = (*env)->GetIntArrayElements(env, bSqrs, false);
+	jchar *jWPcs = (*env)->GetCharArrayElements(env, wPcs, false);
+	jchar *jBPcs = (*env)->GetCharArrayElements(env, bPcs, false);
+	unsigned cWSqrs[wLength];
+	unsigned cBSqrs[bLength];
+	unsigned char cWPcs[wLength];
+	unsigned char cBPcs[bLength];
 	for (int i = 0; i < wLength; i++) {
-		cWSqrs[i] = jWSqrs[i];
-		cWPcs[i] = jWPcs[i];
+		cWSqrs[i] = (unsigned) *(jWSqrs + i);
+		cWPcs[i] = (unsigned char) *(jWPcs + i);
 	}
 	for (int i = 0; i < bLength; i++) {
-		cBSqrs[i] = jBSqrs[i];
-		cBPcs[i] = jBPcs[i];
+		cBSqrs[i] = (unsigned) *(jBSqrs + i);
+		cBPcs[i] = (unsigned char) *(jBPcs + i);
 	}
 	(*env)->ReleaseIntArrayElements(env, wSqrs, jWSqrs, 0);
 	(*env)->ReleaseIntArrayElements(env, bSqrs, jBSqrs, 0);
-	(*env)->ReleaseIntArrayElements(env, wPcs, jWPcs, 0);
-	(*env)->ReleaseIntArrayElements(env, bPcs, jBPcs, 0);
-	return;
+	(*env)->ReleaseCharArrayElements(env, wPcs, jWPcs, 0);
+	(*env)->ReleaseCharArrayElements(env, bPcs, jBPcs, 0);
+	struct BOARD_POS pos;
+	pos.cWSqrs = cWSqrs;
+	pos.cBSqrs = cBSqrs;
+	pos.cWPcs = cWPcs;
+	pos.cBPcs = cBPcs;
+	return pos;
+}
+
+jintArray getProbeResult(JNIEnv * env, unsigned info, unsigned dtm) {
+	jintArray jArray =  (*env)->NewIntArray(env, 2);
+	jint res[] = { info, dtm };
+	(*env)->SetIntArrayRegion(env, jArray, 0 , 2, res);
+	return jArray;
 }
